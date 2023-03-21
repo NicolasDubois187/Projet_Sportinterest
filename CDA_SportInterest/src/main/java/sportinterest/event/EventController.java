a93867dc2512a13798b341dd16d75233fb8f4f32
nicolasDubois <nicolasdubois187.pro@gmail.com>
2023-03-21 23:31:45 +0100

    first_config

--- CDA_SportInterest/src/main/java/sportinterest/event/EventController.java
+++ CDA_SportInterest/src/main/java/sportinterest/event/EventController.java
@@ -1,11 +1,78 @@
 package sportinterest.event;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
 
 @RestController
 public class EventController {
 	
 	@Autowired
 	EventService eventService;
+	
+/**
+ * get all events	
+ * @return
+ */
+    @GetMapping("events")
+    public List<Event> getEvents(){
+        return eventService.getEvents();
+    }
+/**
+ * add one event
+ * @param newEvent
+ */
+    @PostMapping("events")
+    public void postEvent(@RequestBody Event newEvent){
+		    
+        eventService.addEvent(newEvent);
+    }
+/**
+ * get one event by his id    
+ * @param id
+ * @return
+ */
+    @GetMapping("events/{id}")
+    public ResponseEntity<Event> getOneEvent(@PathVariable("id") Integer id){
+    	Optional<Event> oEvent = eventService.getOneEvent(id);
+    	if(oEvent.isEmpty()) {
+    		// 404
+    		return ResponseEntity.notFound().build();
+    	} else {
+    		Event e = oEvent.get();
+    		return ResponseEntity.ok(e);
+    	}
+    }
+/**
+ * update one event by his id    
+ * @param id
+ * @param event
+ */
+    @PutMapping("events/{id}")
+    public void updateEvent(@PathVariable("id") Integer id, @RequestBody Event event) {
+    	Optional<Event> oEvent = eventService.getOneEvent(id);
+    	if(oEvent.isPresent()) {
+		    		
+    	eventService.updateEvent(id, event);
+    	}
+    }
+/**
+ * delete one event by his id    
+ * @param id
+ */
+    @DeleteMapping("events/{id}")
+    public void deleteEvent(@PathVariable("id") Integer id) {
+    	eventService.deleteEvent(id);
+    }
+	
 }
--- CDA_SportInterest/src/main/java/sportinterest/event/EventService.java
+++ CDA_SportInterest/src/main/java/sportinterest/event/EventService.java
@@ -1,11 +1,60 @@
 package sportinterest.event;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
 
 @Service
 public class EventService {
 	
 	@Autowired
 	EventRepository eventRepository;
+	
+/**
+ * 
+ * @return all events
+ */
+	public List<Event> getEvents() {
+				
+		return eventRepository.findAll();
+	}
+/**
+ * add one event	
+ * @param event
+ */
+	public void addEvent(Event event) {
+
+		eventRepository.save(event);
+	}
+/**
+ * get one event by his id	
+ * @param id
+ * @return
+ */
+	public Optional<Event> getOneEvent(Integer id) {
+
+		return eventRepository.findById(id);
+	}
+/**
+ * update attributes of one event
+ * @param id
+ * @param event
+ */
+	public void updateEvent(Integer id, Event event) {
+
+		eventRepository.save(event);
+	}
+/**
+ * Delete one event by his id	
+ * @param id
+ */
+	public void deleteEvent(Integer id) {
+
+		eventRepository.deleteById(id);
+	}	
+		
 }
+
