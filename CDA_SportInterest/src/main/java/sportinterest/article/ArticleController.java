3e14ba41c51c99eab52f6ba62fd90baf9308c2f6
nicolasDubois <nicolasdubois187.pro@gmail.com>
2023-03-21 22:21:26 +0100

    firstConfig

--- CDA_SportInterest/src/main/java/sportinterest/article/ArticleController.java
+++ CDA_SportInterest/src/main/java/sportinterest/article/ArticleController.java
@@ -1,11 +1,18 @@
 package sportinterest.article;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
 
 
 @RestController
@@ -14,8 +21,60 @@ public class ArticleController {
 	@Autowired
 	ArticleService articleService;
 
+/**
+ * get all articles	
+ * @return
+ */
     @GetMapping("articles")
     public List<Article> getArticles(){
         return articleService.getArticles();
     }
+/**
+ * add one article
+ * @param newArticle
+ */
+    @PostMapping("articles")
+    public void postArticle(@RequestBody Article newArticle){
+    
+        articleService.addArticle(newArticle);
+    }
+/**
+ * get one article by his id    
+ * @param id
+ * @return
+ */
+    @GetMapping("articles/{id}")
+    public ResponseEntity<Article> getOneArticle(@PathVariable("id") Integer id){
+    	Optional<Article> oArticle = articleService.getOneArticle(id);
+    	if(oArticle.isEmpty()) {
+    		// 404
+    		return ResponseEntity.notFound().build();
+    	} else {
+    		Article a = oArticle.get();
+    		return ResponseEntity.ok(a);
+    	}
+    }
+/**
+ * update one article by his id    
+ * @param id
+ * @param article
+ */
+    @PutMapping("articles/{id}")
+    public void updateArticle(@PathVariable("id") Integer id, @RequestBody Article article) {
+    	Optional<Article> oArticle = articleService.getOneArticle(id);
+    	if(oArticle.isPresent()) {
+    		
+    		articleService.updateArticle(id, article);
+    	}
+    }
+/**
+ * delete one article by his id    
+ * @param id
+ */
+    @DeleteMapping("articles/{id}")
+    public void deleteArticle(@PathVariable("id") Integer id) {
+    	articleService.deleteArticle(id);
+    }
+    
+    
 }
--- CDA_SportInterest/src/main/java/sportinterest/article/ArticleService.java
+++ CDA_SportInterest/src/main/java/sportinterest/article/ArticleService.java
@@ -1,17 +1,59 @@
 package sportinterest.article;
 
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
 
 @Service
 public class ArticleService {
 
 	@Autowired
 	ArticleRepository articleRepository;
-
+/**
+ * 
+ * @return all articles
+ */
 	public List<Article> getArticles() {
 		
 		return articleRepository.findAll();
 	}
+/**
+ * add one article	
+ * @param article
+ */
+	public void addArticle(Article article) {
+
+		articleRepository.save(article);
+	}
+/**
+ * get one article by his id	
+ * @param id
+ * @return
+ */
+	public Optional<Article> getOneArticle(Integer id) {
+
+		return articleRepository.findById(id);
+	}
+/**
+ * update attributes of one article	
+ * @param id
+ * @param article
+ */
+	public void updateArticle(Integer id, Article article) {
+
+		articleRepository.save(article);
+	}
+/**
+ * Delete one article by his id	
+ * @param id
+ */
+	public void deleteArticle(Integer id) {
+
+		articleRepository.deleteById(id);
+	}
+	
 }
