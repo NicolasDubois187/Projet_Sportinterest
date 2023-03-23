2a78c08e4d34742ab721d5ed5a6f54523b9441b5
nicolasDubois <nicolasdubois187.pro@gmail.com>
2023-03-23 22:49:57 +0100

    first_config

--- CDA_SportInterest/src/main/java/sportinterest/user/UserController.java
+++ CDA_SportInterest/src/main/java/sportinterest/user/UserController.java
@@ -1,11 +1,76 @@
 package sportinterest.user;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
 
 @RestController
 public class UserController {
 
 	@Autowired
-	UserService userSerice;
+	UserService userService;
+/**
+ * get all users	
+ * @return
+ */
+    @GetMapping("users")
+    public List<User> getUsers(){
+        return userService.getUsers();
+    }
+/**
+ * add one user
+ * @param newUser
+ */
+    @PostMapping("users")
+    public void postUser(@RequestBody User newUser){
+				    
+        userService.addUser(newUser);
+    }
+/**
+ * get one user by his id    
+ * @param id
+ * @return
+ */
+    @GetMapping("users/{id}")
+    public ResponseEntity<User> getOneUser(@PathVariable("id") Integer id){
+    	Optional<User> oUser = userService.getOneUser(id);
+    	if(oUser.isEmpty()) {
+    		// 404
+    		return ResponseEntity.notFound().build();
+    	} else {
+    		User u = oUser.get();
+    		return ResponseEntity.ok(u);
+    	}
+    }
+/**
+ * update one user by his id    
+ * @param id
+ * @param user
+ */
+    @PutMapping("users/{id}")
+    public void updateUser(@PathVariable("id") Integer id, @RequestBody User user) {
+    	Optional<User> oUser = userService.getOneUser(id);
+    	if(oUser.isPresent()) {
+				    		
+    	userService.updateUser(id, user);
+    	}
+    }
+/**
+ * delete one user by his id    
+ * @param id
+ */
+    @DeleteMapping("users/{id}")
+    public void deleteUser(@PathVariable("id") Integer id) {
+    	userService.deleteUser(id);
+    }
 }
